3380247903904b5aa0a70d10630a011f49a9a358
vishal phule <vishalphule2000@gmail.com>
2022-07-30 16:04:11 +0530

    Updated Code

--- force-app/main/default/lwc/courseDetailComponent/courseDetailComponent.js
+++ force-app/main/default/lwc/courseDetailComponent/courseDetailComponent.js
@@ -1,18 +1,47 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import fetchCourseDetails from '@salesforce/apex/CourseDetailLWCService.fetchCourseDetails';
 import fetchTrainerDetails from '@salesforce/apex/CourseDetailLWCService.fetchTrainerDetails';
+import fetchUserName from '@salesforce/apex/UserUtility.fetchUserName';
 
-export default class CourseDetailComponent extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class CourseDetailComponent extends NavigationMixin(LightningElement) {
 
     @api courseId;
     @api source;
     __CurrentPageReference;
+
+    // Variable to show the Spinner
     isSpinner = false;
 
+    // Variable to store Course Details
     __trainers;
     __courseDetails;
     __errors;
+
+    // Variable to Display the location Map
+    @track __mapMarkers = [];
+
+    // Variable for to show/hide RSVP button
+    __showEnrollButton = false;
+
+
+    @wire(fetchUserName)
+    wireDaata({ error, data }) {
+        if (data) {
+            console.log('User Name \nn', data);
+            if (data.includes('Site Guest User')) {
+                this._showEnrollButton = false;
+            }
+            else {
+                this._showEnrollButton = true;
+            }
+        }
+        else if (error) {
+            console.error("error", error);
+        }
+    }
 
     @wire(CurrentPageReference)
     getCurrentPageReference(PageReference) {
@@ -30,10 +59,26 @@ export default class CourseDetailComponent extends LightningElement {
 
     fetchCourseDetailsJS() {
         this.isSpinner = true;
-        fetchCourseDetails({ recordId: this.courseId })
+        fetchCourseDetails({
+            recordId: this.courseId
+        })
             .then(result => {
                 console.log('Result', result);
                 this.__courseDetails = result;
+
+                if (this.__eventDetails.Location__c) {
+                    this.__mapMarkers.push({
+                        location: {
+                            City: this.__courseDetails.Location__r.City__c,
+                            Country: this.__courseDetails.Location__r.Country__c,
+                            PostalCode: this.__courseDetails.Location__r.Postal_Code__c,
+                            State: this.__courseDetails.Location__r.State__c,
+                            Street: this.__courseDetails.Location__r.Street__c
+                        },
+                        title: this.__courseDetails.Name__c,
+                        description: 'This is the landmark for the location'
+                    });
+                }
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -42,7 +87,6 @@ export default class CourseDetailComponent extends LightningElement {
             .finally(() => {
                 this.isSpinner = false;
             });
-
     }
 
     fetchTrainerDetailsJS() {
@@ -61,12 +105,24 @@ export default class CourseDetailComponent extends LightningElement {
                 this.isSpinner = false;
             });
     }
-
-    handleRSVP() {
-
+    handleEnroll() {
+        console.log('OUTPUT:', this.eventId);
     }
+
     handleContactUs() {
 
+    }
+
+    handleLoginRedirect() {
+        let navigationTarget = {
+            type: 'comm__namedPage',
+            attributes: {
+                name: "Login"
+            }
+
+        }
+        //   this.__CurrentPageReference.Navigate(navigationTarget);
+        alert('Login to site to Enroll the Course');
     }
 
 }
